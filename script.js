e4aabdb3a911d9b56b99a50af867786414953798
Anurag Bansal <devanuragbansal@gmail.com>
2025-02-11 02:32:13 +0530

    Footer Completed

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
-const timeElement = document.getElementById('time');
+const timeElement = document.querySelectorAll('.time');
+const dateElement = document.querySelectorAll('.date');
 
 setInterval(() => {
-    const date = new Date();
-    let hours = date.getHours();
-    const minutes = date.getMinutes().toString().padStart(2, '0');
+    const now = new Date();
+    let hours = now.getHours();
+    const minutes = now.getMinutes().toString().padStart(2, '0');
     const period = hours >= 12 ? 'PM' : 'AM';
+    const date = now.getDate();
+    const month = now.toLocaleString('default', { month: 'long' });
+    const year = now.getFullYear();
 
     hours = hours % 12 || 12;
     hours = hours.toString().padStart(2, '0');
 
     const time = `${hours}:${minutes} ${period}`;
+    const dateString = `${date} ${month} ${year}`;
     
-    timeElement.textContent = time;
+    timeElement.forEach(element => {
+        element.textContent = time;
+    });
+
+    dateElement.forEach(element => {
+        element.textContent = dateString;
+    });
+ 
 }, 1000);
